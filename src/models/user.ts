2139dab4562f17b8ccb037a9e2d241f6d1c8cba3
Ray Traina <racheltrainagrandon@gmail.com>
2024-10-06 19:19:11 -0700

    Initial user auth flow next is jwt

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt';
 import client from '../database';
 
 export type User = {
@@ -42,6 +43,8 @@ export class UserQueries {
     isActive: boolean = true,
   ): Promise<User[]> {
     try {
+      // TODO create pw hash + pepper here
+      const hash = bcrypt.hashSync(password, 10); //10 salt rounds for test
       const connection = await client.connect();
       const sql =
         'INSERT INTO users (firstName, lastName, email, password, isActive) VALUES ($1, $2, $3, $4, $5)';
@@ -49,14 +52,35 @@ export class UserQueries {
         firstName,
         lastName,
         email,
-        password,
+        hash,
         isActive,
       ]);
-      const newUser = result.rows[0];
+      console.log(result);
       connection.release();
-      return newUser;
+      const authenticatedUser = this.authenticate(email, password);
+      return authenticatedUser;
     } catch (error) {
       throw new Error(`${error}`);
+    }
+  }
+
+  async authenticate(email:string, password:string ) {
+    try {    
+      const connection = await client.connect();
+      const sql = 'SELECT * FROM users WHERE email=($1)';
+      const result = await connection.query(sql, [email]);
+      if(result.rows.length) {
+        const user = result.rows[0];
+        console.log(user);
+        if (bcrypt.compareSync(password, user.password)) {
+          return user;
+        } else {
+          return null;
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      throw new Error(`Could not authenticate user with this email: ${email}`);
     }
   }
 }
